91aed6d2fda4ec09f59cde6fd534050a6445cfce
rensawamo <ren0112ngzk@gmail.com>
2024-01-26 19:10:17 +0900

    intaractive 追加

---
+++ interactive/lib/widgets/expenses_list/expense_item.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+import '../../models/expense.dart';
+
+class ExpenseItem extends StatelessWidget {
+  const ExpenseItem(this.expense, {super.key});
+
+  final Expense expense;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        // 上下 左右 のpadding のつけ方などが可能
+        padding: const EdgeInsets.symmetric(
+          horizontal: 10,
+          vertical: 16,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          // defalult vstack
+          children: [
+            Text(
+              expense.title,
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
+            const SizedBox(height: 11), // spacer に近い
+            Row(
+              children: [
+                Text(
+                  '\$${expense.amount.toStringAsFixed(2)}',
+                ),
+                const Spacer(),
+                Row(
+                  children: [
+                    Icon(categoryIcons[expense.category]),
+                    const SizedBox(width: 8),
+                    Text(expense.formattedDate),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
