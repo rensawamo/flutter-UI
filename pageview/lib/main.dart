2ec2579b5e6764edc1d4d9001048c9b237c93eb5
rensawamo <ren0112ngzk@gmail.com>
2024-02-01 18:52:14 +0900

    画面スクロール

--- pageview/lib/main.dart
+++ pageview/lib/main.dart
@@ -11,12 +11,8 @@ void main() {
 
 /// 馬のモデルクラス
 class Horse {
-  // 名前
   final String name;
-  // アイコン画像
   final String iconUri;
-
-  // コンストラクタ
   Horse(this.name, this.iconUri);
 }
 
@@ -35,7 +31,6 @@ class HorseCard extends StatelessWidget {
     super.key,
     required this.model,
   });
-  // データが入ったモデル
   final Horse model;
 
   @override
@@ -54,7 +49,6 @@ class HorseCard extends StatelessWidget {
       style: const TextStyle(fontSize: 20),
     );
 
-    // 画像と名前を縦に並べる
     final imgAndText = Column(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: [
@@ -66,17 +60,14 @@ class HorseCard extends StatelessWidget {
     // カード部分を作るコンテナ
     return Container(
       decoration: BoxDecoration(
-        // 色
-        color: Colors.orange,
-        // 角丸
+        color: Colors.red,
         borderRadius: BorderRadius.circular(20),
-        // 影
         boxShadow: [
           BoxShadow(
             // 影の設定
             color: Colors.black.withOpacity(0.2), //色
-            spreadRadius: 2, // 広がりの大きさ
-            blurRadius: 2, // ぼかしの強さ
+            spreadRadius: 20, // 広がりの大きさ
+            blurRadius: 11, // ぼかしの強さ
             offset: const Offset(0, 2), // 方向
           ),
         ],
@@ -86,16 +77,13 @@ class HorseCard extends StatelessWidget {
   }
 }
 
-// モデル => ウィジェット に変換する関数
+// Wigtet は widgetの変数宣言
+// パラムの受け取りが可能
 Widget modelToWidget(Horse model) {
-  // ページの部分
   return Container(
-    // カードの周りに 10 ずつスペースを空ける
     padding: const EdgeInsets.all(10),
-    // 中身は カード
     child: HorseCard(model: model),
   );
-  // カードを使う
 }
 
 // ホーム画面
@@ -104,24 +92,17 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // カルーセル
-    final carousel = PageView.builder(
-      // カルーセルのコントローラー
+    final carousel = PageView.builder( // 横スクロールが可能
       controller: PageController(
-        // 左右のカードがどのくらい見えるかのバランスを決める
         viewportFraction: 0.6,
       ),
-      // カードの数 = モデルの数
       itemCount: models.length,
-      // モデルをWidgetに変換する関数
       itemBuilder: (c, i) => modelToWidget(models[i]),
     );
 
     // 画面
     return Scaffold(
-      // 真ん中
       body: Center(
-        // 横長のコンテナ
         child: Container(
           height: 200,
           color: Colors.white,
